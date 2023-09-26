718779f8c5a0b84ff543b2eebc634a3910adf1b5
bfsylvain <sylvainbonnaure@hotmail.com>
2023-09-26 11:43:17 +0200

    sidebar work in progress

--- script.js
+++ script.js
@@ -8,6 +8,59 @@ let sidebarRight = document.querySelector('.sidebar-right')
 let profilImg = document.querySelector('.user-img')
 let sidebarLeft = document.querySelector('.sidebar-left')
 
+let messageArea = document.querySelector('.message-area')
+
+const messageSenders = [
+    {
+        date: "hier",
+        firstname: "John", 
+        lastname: "bob",
+        message: "Je suis un prince namibien et j’ai besoin de votre aide..."
+    },
+    {
+        date: "hier",
+        firstname: "John", 
+        lastname: "John",
+        message: "Banana Banana Banana Banana Banana"
+    },
+    {
+        date: "hier",
+        firstname: "John", 
+        lastname: "Bon",
+        message: "Banana Banana Banana Banana Banana"
+    }
+]
+
+for (let messageSender of messageSenders) {
+    const message = document.createElement('div')
+    message.classList.add('message')
+    const profile = document.createElement('div')
+    profile.classList.add('profile')
+    const senderImg = document.createElement('img')
+    senderImg.classList.add('sender-img')
+    senderImg.setAttribute('src', 'assets/img/WF Image Placeholder.png')
+    const sender = document.createElement('div')
+    sender.classList.add('sender')
+    const senderName = document.createElement('p')
+    senderName.classList.add('sender-name')
+    senderName.innerText = `${messageSender.firstname} ${messageSender.lastname}`
+    const senderTime = document.createElement('p')
+    senderTime.innerText = messageSender.date
+    const msgTxt = document.createElement('div')
+    msgTxt.classList.add('message-text')
+    const text = document.createElement('p')
+    text.classList.add('text')
+    text.innerText = messageSender.message
+    messageArea.appendChild(message)
+    message.appendChild(profile)
+    profile.appendChild(senderImg)
+    profile.appendChild(sender)
+    sender.appendChild(senderName)
+    sender.appendChild(senderTime)
+    message.appendChild(msgTxt)
+    msgTxt.appendChild(text)
+}
+
 
 messageBtn.addEventListener('click', () => {
     sidebarRight.classList.toggle('active-right')
@@ -37,7 +90,7 @@ const likeIcons = document.querySelectorAll(".like-img")
 
  for (let likeIcon of likeIcons) {
      likeIcon.addEventListener("click", (event) => {
-        console.log(event.target)
+        //console.log(event.target)
         event.target.strokeStyle= "red"
          const likeZone = event.target.parentNode
          const likeCounter = likeZone.querySelector("span")
