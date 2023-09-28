e1e1f4a17788d39398e2cc51e9e3d853020dff82
Antoine Cairey <cairey.antoine@gmail.com>
2023-09-28 11:51:07 +0200

    Ajout de commentaires et points-virgules

--- functions.js
+++ functions.js
@@ -1,89 +1,95 @@
+// FICHIER JS CONTENANT LES FONCTIONS
 
+// Fonction de création des articles (messages et commentaires)
+// paramètres : contenu du message (objet JS) et noeud HTML parent
 export function createArticle(objet, parent) {
-    const message = document.createElement('div')
-    message.classList.add('message')
-    const profile = document.createElement('div')
-    profile.classList.add('profile')
-    const senderImg = document.createElement('img')
-    senderImg.classList.add('sender-img')
-    senderImg.setAttribute('src', 'assets/img/WF Image Placeholder.png')
-    const sender = document.createElement('div')
-    sender.classList.add('sender')
-    const senderName = document.createElement('p')
-    senderName.classList.add('sender-name')
-    senderName.innerHTML = `${objet.firstname} ${objet.lastname}`
-    const senderTime = document.createElement('p')
-    senderTime.innerHTML = objet.date
-    const msgTxt = document.createElement('div')
-    msgTxt.classList.add('message-text')
-    const text = document.createElement('p')
-    text.classList.add('text')
-    text.innerText = objet.message
-    parent.appendChild(message)
-    message.appendChild(profile)
-    profile.appendChild(senderImg)
-    profile.appendChild(sender)
-    sender.appendChild(senderName)
-    sender.appendChild(senderTime)
-    message.appendChild(msgTxt)
-    msgTxt.appendChild(text)
+  const message = document.createElement("div");
+  message.classList.add("message");
+  const profile = document.createElement("div");
+  profile.classList.add("profile");
+  const senderImg = document.createElement("img");
+  senderImg.classList.add("sender-img");
+  senderImg.setAttribute("src", "assets/img/WF Image Placeholder.png");
+  const sender = document.createElement("div");
+  sender.classList.add("sender");
+  const senderName = document.createElement("p");
+  senderName.classList.add("sender-name");
+  senderName.innerHTML = `${objet.firstname} ${objet.lastname}`;
+  const senderTime = document.createElement("p");
+  senderTime.innerHTML = objet.date;
+  const msgTxt = document.createElement("div");
+  msgTxt.classList.add("message-text");
+  const text = document.createElement("p");
+  text.classList.add("text");
+  text.innerText = objet.message;
+  parent.appendChild(message);
+  message.appendChild(profile);
+  profile.appendChild(senderImg);
+  profile.appendChild(sender);
+  sender.appendChild(senderName);
+  sender.appendChild(senderTime);
+  message.appendChild(msgTxt);
+  msgTxt.appendChild(text);
 }
 
+// Fonction de création du profil utilisateur (barre latérale gauche) à partir de l'objet contenant le profil
 export function createProfile(objet, parent) {
-    const message = document.createElement('div')
-    message.classList.add('message')
-    const profile = document.createElement('div')
-    profile.classList.add('profile')
-    const senderImg = document.createElement('img')
-    senderImg.classList.add('sender-img')
-    senderImg.setAttribute('src', 'assets/img/WF Image Placeholder.png')
-    const sender = document.createElement('div')
-    sender.classList.add('sender')
-    const senderName = document.createElement('p')
-    senderName.classList.add('sender-name')
-    senderName.innerText = `${objet.firstname} ${objet.lastname}`
-    const msgTxt = document.createElement('div')
-    msgTxt.classList.add('message-text')
-    const abonnements = document.createElement('p')
-    abonnements.classList.add('text')
-    abonnements.innerText = `${objet.abonnements} abonnements`
-    const abonnes = document.createElement('p')
-    abonnes.classList.add('text')
-    abonnes.innerHTML = `${objet.abonnes} abonnés`
-    parent.appendChild(message)
-    message.appendChild(profile)
-    profile.appendChild(senderImg)
-    profile.appendChild(sender)
-    sender.appendChild(senderName)
-    message.appendChild(msgTxt)
-    msgTxt.appendChild(abonnements)
-    msgTxt.appendChild(abonnes)
+  const message = document.createElement("div");
+  message.classList.add("message");
+  const profile = document.createElement("div");
+  profile.classList.add("profile");
+  const senderImg = document.createElement("img");
+  senderImg.classList.add("sender-img");
+  senderImg.setAttribute("src", "assets/img/WF Image Placeholder.png");
+  const sender = document.createElement("div");
+  sender.classList.add("sender");
+  const senderName = document.createElement("p");
+  senderName.classList.add("sender-name");
+  senderName.innerText = `${objet.firstname} ${objet.lastname}`;
+  const msgTxt = document.createElement("div");
+  msgTxt.classList.add("message-text");
+  const abonnements = document.createElement("p");
+  abonnements.classList.add("text");
+  abonnements.innerText = `${objet.abonnements} abonnements`;
+  const abonnes = document.createElement("p");
+  abonnes.classList.add("text");
+  abonnes.innerHTML = `${objet.abonnes} abonnés`;
+  parent.appendChild(message);
+  message.appendChild(profile);
+  profile.appendChild(senderImg);
+  profile.appendChild(sender);
+  sender.appendChild(senderName);
+  message.appendChild(msgTxt);
+  msgTxt.appendChild(abonnements);
+  msgTxt.appendChild(abonnes);
 }
-let header = document.querySelector('.header')
-let articleArea = document.querySelector('.article-area')
-let footer = document.querySelector('.footer')
+
+// Gestion du "shadow mode" : grise le reste de l'écran quand une popup ou sidebar est ouverte
+
+let header = document.querySelector(".header");
+let articleArea = document.querySelector(".article-area");
+let footer = document.querySelector(".footer");
 
 export function removeShadowMode1() {
-    header.classList.remove('shadowMode');
-    articleArea.classList.remove('shadowMode');
-    footer.classList.remove('shadowMode');
+  header.classList.remove("shadowMode");
+  articleArea.classList.remove("shadowMode");
+  footer.classList.remove("shadowMode");
 }
 
 export function removeShadowMode() {
-    header.classList.remove('shadowMode2');
-    articleArea.classList.remove('shadowMode2');
-    footer.classList.remove('shadowMode2');
+  header.classList.remove("shadowMode2");
+  articleArea.classList.remove("shadowMode2");
+  footer.classList.remove("shadowMode2");
 }
 
 export function addShadowMode() {
-    header.classList.add('shadowMode2');
-    articleArea.classList.add('shadowMode2');
-    footer.classList.add('shadowMode2');
+  header.classList.add("shadowMode2");
+  articleArea.classList.add("shadowMode2");
+  footer.classList.add("shadowMode2");
 }
 
 export function shadowModeToggle() {
-    header.classList.toggle('shadowMode')
-    articleArea.classList.toggle('shadowMode')
-    footer.classList.toggle('shadowMode')
+  header.classList.toggle("shadowMode");
+  articleArea.classList.toggle("shadowMode");
+  footer.classList.toggle("shadowMode");
 }
-
--- script.js
+++ script.js
@@ -1,156 +1,176 @@
+// FICHIER JS PRINCIPAL
 
-import { createArticle, createProfile,  removeShadowMode1, removeShadowMode, addShadowMode, shadowModeToggle} from "./functions.js"
+// Import des fonctions depuis functions.js
+import {
+  createArticle,
+  createProfile,
+  removeShadowMode1,
+  removeShadowMode,
+  addShadowMode,
+  shadowModeToggle,
+} from "./functions.js";
 
+// Récupération des noeuds HTML dans des variables JS
+const header = document.querySelector(".header");
+const articleArea = document.querySelector(".article-area");
+const footer = document.querySelector(".footer");
 
-const header = document.querySelector('.header')
-const articleArea = document.querySelector('.article-area')
-const footer = document.querySelector('.footer')
+const messageBtn = document.querySelector(".message-img");
+const profileBtn = document.querySelector(".user-img");
 
-const messageBtn = document.querySelector('.message-img')
-const profileBtn = document.querySelector('.user-img')
-
-const likeBtns = document.querySelectorAll(".like-img")
+const likeBtns = document.querySelectorAll(".like-img");
 const commentBtn = document.querySelectorAll(".comment-img");
 
 const commentContainer = document.querySelector(".comment-container");
-const lastComments = document.querySelectorAll(".last-comment")
-console.log(lastComments)
+const lastComments = document.querySelectorAll(".last-comment");
+console.log(lastComments);
 const cancelBtn = document.querySelector(".cancel");
-const submitBtn = document.querySelector(".submit")
+const submitBtn = document.querySelector(".submit");
 
-const sidebarRight = document.querySelector('.sidebar-right')
-const sidebarLeft = document.querySelector('.sidebar-left')
+const sidebarRight = document.querySelector(".sidebar-right");
+const sidebarLeft = document.querySelector(".sidebar-left");
 
-const messageArea = document.querySelector('.message-area')
+const messageArea = document.querySelector(".message-area");
 
-const profile = document.querySelector('.user-profile')
+const profile = document.querySelector(".user-profile");
 
-const commentContainerContent = document.querySelector(".comment-container-content")
+const commentContainerContent = document.querySelector(
+  ".comment-container-content"
+);
 
+// Création d'un tableau d'objets représentant les messages du site
 const messageSenders = [
-    {
-        date: "hier",
-        firstname: "Cédric", 
-        lastname: "D",
-        message: "Apéro ?"
-    },
-    {
-        date: "hier",
-        firstname: "John", 
-        lastname: "John",
-        message: "Banana Banana Banana Banana Banana"
-    },
-    {
-        date: "hier",
-        firstname: "John", 
-        lastname: "Bob",
-        message: "Banana Banana Banana Banana Banana"
-    }
-]
+  {
+    date: "hier",
+    firstname: "Cédric",
+    lastname: "D",
+    message: "Apéro ?",
+  },
+  {
+    date: "hier",
+    firstname: "John",
+    lastname: "John",
+    message: "Banana Banana Banana Banana Banana",
+  },
+  {
+    date: "hier",
+    firstname: "John",
+    lastname: "Bob",
+    message: "Banana Banana Banana Banana Banana",
+  },
+];
 
+// Création de l'objet "profil de l'utilisateur"
 const user = {
-    firstname: "Mr",
-    lastname: "Fantastic",
-    abonnes: 1961,
-    abonnements: 3
-}
+  firstname: "Mr",
+  lastname: "Fantastic",
+  abonnes: 1961,
+  abonnements: 3,
+};
 
-createProfile(user, profile)
+// Affichage du profil utilisateur (barre latérale gauche)
+createProfile(user, profile);
 
+// Affichage des messages (barre latérale droite)
 for (let messageSender of messageSenders) {
-    createArticle(messageSender, messageArea)
+  createArticle(messageSender, messageArea);
 }
 
+// Affichage des commentaires (popup en bas)
 for (let messageSender of messageSenders) {
-    createArticle(messageSender, commentContainerContent)
+  createArticle(messageSender, commentContainerContent);
 }
 
+// Afficher ou masquer le header en fonction du scroll
 let lastScroll = 0;
-
 window.addEventListener("scroll", () => {
-    if(window.scrollY < lastScroll) {
-        header.style.top = 0;
-    } else {
-        header.style.top = "-8vh";
-        //tentative
-        sidebarLeft.classList.remove('active-left')
-        sidebarRight.classList.remove('active-right')
-        removeShadowMode1()
+  if (window.scrollY < lastScroll) {
+    header.style.top = 0;
+  } else {
+    header.style.top = "-8vh";
+    //tentative
+    sidebarLeft.classList.remove("active-left");
+    sidebarRight.classList.remove("active-right");
+    removeShadowMode1();
+  }
+  lastScroll = window.scrollY;
+});
 
-    }
-    lastScroll = window.scrollY;
-})
+// Le bouton "messages" ouvre la barre latérale droite
+messageBtn.addEventListener("click", () => {
+  sidebarRight.classList.toggle("active-right");
+  shadowModeToggle();
+});
 
-messageBtn.addEventListener('click', () => {
-    sidebarRight.classList.toggle('active-right')
-    shadowModeToggle()
-})
+// Le bouton "profil" ouvre la barre latérale gauche
+profileBtn.addEventListener("click", () => {
+  sidebarLeft.classList.toggle("active-left");
+  shadowModeToggle();
+  header.classList.toggle("shadowMode");
+});
 
-profileBtn.addEventListener('click', () => {
-    sidebarLeft.classList.toggle('active-left')
-    shadowModeToggle()
-    header.classList.toggle('shadowMode')
-})
+// Fermer la sidebar si on clique en dehors de la sidebar
+document.addEventListener("click", (e) => {
+  if (
+    !sidebarRight.contains(e.target) &&
+    e.target !== messageBtn &&
+    e.target !== profileBtn
+  ) {
+    sidebarLeft.classList.remove("active-left");
+    sidebarRight.classList.remove("active-right");
+    removeShadowMode1();
+  }
+});
 
-document.addEventListener('click', e => {
-    if(!sidebarRight.contains(e.target) && e.target !== messageBtn && e.target !== profileBtn) {
-        sidebarLeft.classList.remove('active-left')
-        sidebarRight.classList.remove('active-right')
-        removeShadowMode1()
-    }
-})
-
+// Incrémente ou décrémente le nombre de likes quand on clique sur l'icone dédiée
 const library = new Set();
-
 likeBtns.forEach((likeBtn, key) => {
-    likeBtn.addEventListener("click", (event) => {
-        const likeZone = event.target.parentNode
-        const likeCounter = likeZone.querySelector("span")
-        if (library.has(key)) {
-            likeCounter.innerHTML--
-            library.delete(key)
-        } else {
-        event.target.strokeStyle= "red"
-        likeCounter.innerHTML++
-        library.add(key);
-        }
-     })
-})
+  likeBtn.addEventListener("click", (event) => {
+    const likeZone = event.target.parentNode;
+    const likeCounter = likeZone.querySelector("span");
+    if (library.has(key)) {
+      likeCounter.innerHTML--;
+      library.delete(key);
+    } else {
+      event.target.strokeStyle = "red";
+      likeCounter.innerHTML++;
+      library.add(key);
+    }
+  });
+});
 
+// Affiche la popup "commentaires" quand on clique sur l'icone dédiée
 const commentsLibrary = new Set();
-
 commentBtn.forEach((button, index) =>
-    button.addEventListener('click', () => {
-    commentsLibrary.add(index)
+  button.addEventListener("click", () => {
+    commentsLibrary.add(index);
     commentContainer.classList.add("showComment");
-    addShadowMode()
-    })
-)
+    addShadowMode();
+  })
+);
 
-cancelBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    commentContainer.classList.remove("showComment");
-    removeShadowMode();
-    commentInput.value = ""
-    commentsLibrary.clear()
-})
+// Masque la popup "commentaires" quand on clique sur "Annuler"
+cancelBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  commentContainer.classList.remove("showComment");
+  removeShadowMode();
+  commentInput.value = "";
+  commentsLibrary.clear();
+});
 
-let commentInput = document.querySelector(".comment-txt")
-submitBtn.addEventListener("click", (e) =>  {
-    e.preventDefault();
-    const commentsZoneTarget = commentBtn[[...commentsLibrary][0]].parentNode
-    const commentsCounter = commentsZoneTarget.querySelector("span")
-    commentsCounter.innerHTML++
-    commentContainer.classList.remove("showComment");
-    removeShadowMode()
-    console.log(commentInput.value)
-    let newComment = commentInput.value
-    user.message = newComment
-    createArticle(user, lastComments[[...commentsLibrary][0]])
-    commentInput.value = ""
-    commentsLibrary.clear()
-})
-
-
-
-
+// Crée le commentaire quand on clique sur "Envoyer"
+let commentInput = document.querySelector(".comment-txt");
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const commentsZoneTarget = commentBtn[[...commentsLibrary][0]].parentNode;
+  const commentsCounter = commentsZoneTarget.querySelector("span");
+  commentsCounter.innerHTML++;
+  commentContainer.classList.remove("showComment");
+  removeShadowMode();
+  console.log(commentInput.value);
+  let newComment = commentInput.value;
+  user.message = newComment;
+  createArticle(user, lastComments[[...commentsLibrary][0]]);
+  commentInput.value = "";
+  commentsLibrary.clear();
+});
