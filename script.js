50a3eee66a4c6c029a26bbaf153162a330d0d2e6
VICTOR GUYOMARD <victorguyo44600@gmail.com>
2023-09-29 10:15:02 +0200

    Fix merge

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+// Animation de la barre nav lors d'un scroll //
 const navigation = document.querySelector("header.header");
-window.addEventListener('scroll', (e) => {
-    console.log(window.scrollY);
+window.addEventListener("scroll", (e) => {
+  console.log(window.e);
+});
+
+/* window.addEventListener('scroll', () => {
 
     if (window.scrollY > 120) {
         navigation.style.top = ("-70px");
     } else { 
         navigation.style.top = 0;
 }
+}); */
+
+// Animation du pop up Message //
+
+const message = document.querySelector(".mail");
+const popUpMessage = document.querySelector(".popUpMessage");
+
+message.addEventListener("click", () => {
+  popUpMessage.style.left = "150px";
 });
-
-
